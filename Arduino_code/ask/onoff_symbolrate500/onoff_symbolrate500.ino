36f4e0761f9867e1d696cddebd8d0448a090b8ca
nuakojr <41984741+nuakojr@users.noreply.github.com>
2022-09-14 01:28:39 -0400

    Add files via upload

---
+++ Arduino_code/ask/onoff_symbolrate500/onoff_symbolrate500.ino
@@ -0,0 +1,59 @@
+
+//This code Transmitts a signal of all 1 and 0 bits
+//1010101010
+//8-17-2022 Hongzhi Guo
+#include<stdlib.h>
+#include<stdbool.h>
+#include<stdio.h>
+#include<math.h>
+void setup() {
+  // put your setup code here, to run once:
+pinMode(11, OUTPUT);
+Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int input = 13146;
+int a[20];
+
+    for(int i=19;i>=0;i--){
+      if (i>15){
+        onehightolow();
+        }
+      else{a[i]=input%2;
+        input = input/2;
+        if(a[i]==0){
+          zerolowlow();
+         }
+        else{
+           onehightolow();
+            }
+        }
+      }
+   delayMicroseconds(200);
+}
+
+//one bit is high to low
+void onehightolow(){
+  digitalWrite(11, HIGH);
+  //delay(1);
+  delayMicroseconds(1000);
+  digitalWrite(11, LOW);
+  //delay(1);
+  delayMicroseconds(1000);
+}
+
+//zero bit is high to low
+void zerolowlow(){
+  digitalWrite(11,LOW);
+  //delay(1);
+  delayMicroseconds(1000);
+  //digitalWrite(11,HIGH);
+  //delay(1);
+  delayMicroseconds(1000);
+}
+
+// 1 > .9
+//This code is not perfect because it does not produce a perfect square wave
